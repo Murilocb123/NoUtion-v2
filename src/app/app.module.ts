7f898b17c70e6bf45022495819fad8102f8fe561
unknown <murilocb1276@gmail.com>
2024-10-07 21:48:47 -0300

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +11,7 @@ import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 import { ViewsModule } from './views/views.module';
 import { NotesTabComponent } from './views/notes-tab/notes-tab.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,13 @@ import { NotesTabComponent } from './views/notes-tab/notes-tab.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     RippleModule,
-    ViewsModule],
+    ViewsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })],
   providers: [provideAnimations()],
   bootstrap: [AppComponent]
 })
